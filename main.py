6410bfab13adc9704f4a4a42710ec333caee465f
I-like-pelmeni <maxtoo2017@gmail.com>
2022-06-16 19:01:31 +0300

    Add quiz_db

--- main.py
+++ main.py
@@ -33,7 +33,31 @@ async def action_cancel(message: types.Message) -> None:
     await message.answer('Действие отменено. Введите /start, чтобы начать заново',
         reply_markup=types.ReplyKeyboardMarkup())
 
+@dp.message_handler(content_types=['poll'])
+async def msg_with_poll(message: types.Message) -> None:
+    """Message with poll(quiz handler"""
+    user_id = str(message.from_user.id)
+    # if user is unknown
+    if not quiz_db.get(user_id):
+        quiz_db[user_id] = []
 
+    # quiz type check
+    if message.poll.type != 'quiz':
+        await message.reply('Извините, я принимаю только тесты!')
+        return
+
+    quiz_db[user_id].append(Quiz(
+        quiz_id=message.poll.id,
+        question=message.poll.question,
+        options=[option.text for option in message.poll.options],
+        correct_option_id=message.poll.correct_option_id,
+        owner_id=user_id
+    ))
+    quiz_owners[message.poll.id] = user_id
+
+    await message.reply(
+        f'Тест сохранен. Общие количество тестов: {len(quiz_db[user_id])}'
+    )
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
